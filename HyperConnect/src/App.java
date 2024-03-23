f0b20e23d46a46d742bfa9b690ad7d1d07984941
Gabriel <gabrielsirtori.correa@gmail.com>
2024-03-23 11:05:00 -0300

    pathsEditados

--- HyperConnect/src/App.java
+++ HyperConnect/src/App.java
@@ -14,10 +14,11 @@ public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLayout = new FXMLLoader(getClass().getResource("./view/homeLayout.fxml"));
+        //HyperConnect\src\view\homeLayout.fxml
+        FXMLLoader fxmlLayout = new FXMLLoader(getClass().getResource("/view/homeLayout.fxml"));
         Parent root = fxmlLayout.load();
         Scene scene = new Scene(root);
-        Image icon = new Image(getClass().getResourceAsStream("./view/Resources/Icon.png"));
+        Image icon = new Image(getClass().getResourceAsStream("/view/Resources/Icon.png"));
 
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("HYPERCONNECT");
